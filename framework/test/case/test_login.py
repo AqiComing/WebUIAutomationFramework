a819dd2098f6a75fa657be761b83a80705c9e889
AqiComing <317401018@qq.com>
2019-03-12 18:34:22 +0800

    modify

---
+++ framework/test/case/test_login.py
@@ -0,0 +1,22 @@
+import unittest
+
+from test.page.baidu_main_page import BaiDuMainPage
+from test.page.login_page import JDLoginPage
+from utils.config import Config, DRIVER_PATH, DATA_PATH, REPORT_PATH
+
+class TestLogin(unittest.TestCase):
+    URL = Config().get('local_url')
+    Account = Config().get('account')
+    Psw = Config().get('password')
+
+    def sub_setUp(self):
+        # 初始页面是main page，传入浏览器类型打开浏览器
+        self.page = JDLoginPage().get(self.URL, maximize_window=False)
+
+    def test_login(self):
+        self.page = JDLoginPage().get(self.URL, maximize_window=False)
+        self.page.login(self.Account, self.Psw)
+        self.page.sliding_vaildation()
+
+if __name__ == '__main__':
+        unittest.main()
